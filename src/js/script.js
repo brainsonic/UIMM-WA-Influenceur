72eb31dd5a6bfdb1d2dd1402826406c38ce77ea8
AkunesPro <alexis.fiolet@brainsonic.com>
2024-04-18 16:52:17 +0200

    Ajout du tracking de la popup

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@ import { Interaction, InteractAction, Dialog, Modal, ModalAction, PopUpVideo, It
 import { phase_1 } from "./steps/script_phase1";
 import { phase_2 } from "./steps/script_phase2";
 import { phase_3 } from "./steps/script_phase3";
+const url_api = "https://uimm-trackers-api-stg-0b60361e2cd1.herokuapp.com/api/tracker_events";
 
 // VARIABLES ///////////////////////////////////////////////
 const tutorialLink = "https://64ix.github.io/WA-Edited-Tutorial/tutorial.html";
@@ -31,13 +32,49 @@ document.addEventListener('DOMContentLoaded', () => {
         allowApi: !0,
         position: "center",
       });
+      tracking();
   });
 
+  
   phase_1();
   phase_2();
   phase_3();
+  
 
 });
+
+function tracking(){
+      console.log("Nouveau Visiteur");
+      const data = {
+        type: "Popup",
+        category: "Popup",
+        name: "Popup",
+      }
+      const request = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/ld+json'
+        },
+        body: JSON.stringify(data)
+      }
+  
+      fetch(url_api, request)
+        .then(response => {
+          if(!response.ok) {
+            console.log('Erreur tracker');
+            throw new Error('Network response was not OK')
+          }
+          return response.json();
+        })
+        .then(data => {
+  
+          //console.log('Response :', data);
+  
+        })
+        .catch(error => {
+          console.error('Error:', error)
+        }) 
+}
 
 //Log to check in the WA that the script has passed
 console.log('VERSION 11');
